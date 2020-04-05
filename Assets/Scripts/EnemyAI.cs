2ede92de376147ee9da71905c540764ee4f020fe
Alan <alan-mejia@outlook.com>
2020-04-05 18:18:05 -0500

    Walls collision detection fixed
    changed the translate function and instead used the rigidbody velocity to set up the player movement in the x axis

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    float speed = 3.0f;
+    float moveRange = 7.0f;
+    [SerializeField] GameObject target;
+    Animator animController;
+    // Start is called before the first frame update
+    void Start(){
+        animController = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move(){
+        animController.SetBool("isWalking", false);
+        Vector2 distance = target.transform.position - this.transform.position;
+
+        FlipCharacter(distance);     
+
+        if(distance.magnitude < moveRange){
+            this.transform.Translate(distance.normalized * speed * Time.deltaTime);
+            animController.SetBool("isWalking", true);
+        } 
+    }
+
+    private void FlipCharacter(Vector2 distance){
+        float areFacingEachOther = Vector2.Dot(target.transform.right, distance);
+
+        if (Mathf.Abs(areFacingEachOther) > 0)
+            this.transform.localScale = new Vector2(-Mathf.Sign(areFacingEachOther), 1.0f);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,32 +9,36 @@ public class PlayerController : MonoBehaviour
     Animator animController;
     Rigidbody2D rb;
     Collider2D collider2D;
+    BoxCollider2D feet;
     // Start is called before the first frame update
     void Start()
     {
         animController = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         collider2D = GetComponent<Collider2D>();
+        feet = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update(){
         Run();
         Jump();
+        Shoot();
     }
 
     private void Run(){
-        float horizontalMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;   
+        float horizontalMove = Input.GetAxis("Horizontal") * speed;   
         // float verticalMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         FlipCharacter(horizontalMove);
         bool isMoving = Mathf.Abs(horizontalMove) > 0;
         animController.SetBool("isRunning", isMoving);
 
-        this.transform.Translate(horizontalMove, 0, 0);
+        rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
+        // this.transform.Translate(horizontalMove, 0, 0);
     }
 
     private void Jump(){
-        if(!collider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))){
+        if(!feet.IsTouchingLayers(LayerMask.GetMask("Ground"))){
             animController.SetBool("isJumping", true);
             return;
         }
@@ -42,8 +46,11 @@ public class PlayerController : MonoBehaviour
         animController.SetBool("isJumping", false);
         if(Input.GetButtonDown("Jump"))
                 rb.velocity = new Vector2(0.0f, jumpSpeed);
-        }
+    }
 
+    private void Shoot(){
+        
+    }
     private void FlipCharacter(float facingSpeed){
         if(Mathf.Abs(facingSpeed) > 0)
             this.transform.localScale = new Vector2(Mathf.Sign(facingSpeed), 1.0f);
